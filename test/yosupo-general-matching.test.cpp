55cbc442c48198f6df064085d3142396cbd865f8
shen <goodstudyQAQ@163.com>
2024-06-28 11:17:28 +0800

    一般图最大匹配

---
+++ test/yosupo-general-matching.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/general_matching"
+#include <bits/stdc++.h>
+
+#include "../graph/flow/gabow-edmonds.hpp"
+
+using namespace std;
+
+#ifdef LOCAL
+#include "copypaste/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+
+struct fast_ios {
+    fast_ios() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    };
+} fast_ios_;
+
+int main() {
+#ifdef LOCAL
+    freopen("./data.in", "r", stdin);
+#endif
+
+    int N, M;
+    cin >> N >> M;
+    GabowEdmonds flow(N);
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        flow.add_edge(a, b);
+    }
+    auto ret = flow.max_matching();
+    cout << ret.size() << endl;
+    for (auto &p : ret) cout << p.first << ' ' << p.second << endl;
+}
