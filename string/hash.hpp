acd0b539887e42975f21addc992902083e27d946
shen <goodstudyqaq@gmail.com>
2026-01-20 22:52:00 +0100

    fix string hash

--- string/hash.hpp
+++ string/hash.hpp
@@ -5,35 +5,36 @@ typedef unsigned long long ULL;
 /*
 char* 1-bas
 sum[i] = s[1] * Seed ^ 1 + s[2] * Seed ^ 2 + ... s[i] * Seed ^ i
+
 */
 struct Hash {
-    ULL Seed, Mod;
+    long long Seed, Mod;
     string s;
-    vector<ULL> bas, sum;
+    vector<long long> bas, sum;
     vector<int> perm;
 
     int char_size, margin;
     Hash() {}
 
     // int perm[sigma];
-    Hash(ULL Seed, ULL Mod, const string &_s, int _char_size, int _margin) : Seed(Seed), Mod(Mod), s(_s), char_size(_char_size), margin(_margin) {
+    Hash(long long Seed, long long Mod, const string& _s, int _char_size, int _margin) : Seed(Seed), Mod(Mod), s(_s), char_size(_char_size), margin(_margin) {
         int n = s.size();
         bas.resize(n + 1);
         sum.resize(n + 1);
         perm.resize(char_size);
-        iota(perm.begin(), perm.end(), 0);
+        iota(perm.begin(), perm.end(), 1);
         // random_shuffle(perm.begin(), perm.end());
         std::random_device rd;
         std::mt19937 g(rd());
         shuffle(perm.begin(), perm.end(), g);
-        init();
+        // init();
     }
     void init() {
         bas[0] = 1;
         int n = s.size();
         for (int i = 1; i <= n; i++) {
             bas[i] = bas[i - 1] * Seed % Mod;
-            sum[i] = (sum[i - 1] + (s[i - 1] - margin) * bas[i] % Mod) % Mod;
+            sum[i] = (sum[i - 1] + (s[i - 1] - margin + 1) * bas[i] % Mod) % Mod;
         }
     }
     /*random_shuffle 离散化id，防止kill_hash*/
@@ -45,18 +46,19 @@ struct Hash {
             sum[i] = (sum[i - 1] + perm[s[i - 1] - margin] * bas[i] % Mod) % Mod;
         }
     }
-    ULL getHash(int l, int r) {
+    long long getHash(int l, int r) {
         l++, r++;
-        return (sum[r] - sum[l - 1]) * bas[s.size() - l];
+        return ((sum[r] - sum[l - 1]) % Mod + Mod) % Mod * bas[s.size() - l] % Mod;
     }
 };
 const int HASH_CNT = 2;
-ULL Seed_Pool[] = {911, 146527, 19260817, 91815541};
-ULL Mod_Pool[] = {29123, 998244353, 1000000009, 4294967291ull};
+long long Seed_Pool[] = {307, 311, 313, 317, 331, 337, 347, 349};
+long long Mod_Pool[] = {998244353, 998244389, 998244391, 998244397,
+                        998244407, 998244431, 998244433, 998244473};
 
 struct HashHandler {
     vector<Hash> hasher;
-    HashHandler(const string &s, int char_size, int margin) {
+    HashHandler(const string& s, int char_size, int margin) {
         hasher.resize(HASH_CNT);
         for (int i = 0; i < HASH_CNT; i++) {
             hasher[i] = Hash(Seed_Pool[i], Mod_Pool[i], s, char_size, margin);
@@ -64,18 +66,11 @@ struct HashHandler {
         }
     }
 
-    vector<ULL> getHash(int l, int r) {
-        vector<ULL> ans(HASH_CNT);
+    long long getHash(int l, int r) {
+        long long ans = 0;
         for (int i = 0; i < HASH_CNT; i++) {
-            ans[i] = hasher[i].getHash(l, r);
+            ans = (ans << 32) ^ hasher[i].getHash(l, r);
         }
         return ans;
-    }
-    bool equal(const vector<ULL> &h1, const vector<ULL> &h2) {
-        assert(h1.size() == h2.size());
-        for (int i = 0; i < h1.size(); i++) {
-            if (h1[i] != h2[i]) return false;
-        }
-        return true;
     }
 };
